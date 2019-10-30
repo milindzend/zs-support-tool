1293f68aa038a3008082f9e13a7eb7189e5e13b1
milindzend <32719026+milindzend@users.noreply.github.com>
2019-10-30 17:08:33 +0530

    Fixed incorrect path to statistics.ini

--- ZSST_Linux/share/support_tool/actions/zray_off.sh
+++ ZSST_Linux/share/support_tool/actions/zray_off.sh
@@ -3,18 +3,18 @@ ZSST_PLUGIN_NAME="Disable Z-Ray and URL Insight"
 
 
 if [ $NONSU ]; then
-	echo "You need to be superuser to perform this operation."
-	exit 1
+        echo "You need to be superuser to perform this operation."
+        exit 1
 fi
 
 . $ZCE_PREFIX/share/support_tool/st_funcs.lib
 
-INI_FILE="$ZCE_PREFIX/etc/statistics.ini"
+INI_FILE="$ZCE_PREFIX/etc/conf.d/statistics.ini"
 
 if [ "$2" = "--revert" ]; then
-	sed -i "s@; \[ST --zray-off\] zend_extension_manager\.dir\.statistics@zend_extension_manager.dir.statistics@g" $INI_FILE
+        sed -i "s@; \[ST --zray-off\] zend_extension_manager.extension.statistics@zend_extension_manager.extension.statistics@g" $INI_FILE
 else
-	sed -i "s@zend_extension_manager\.dir\.statistics@; [ST --zray-off] zend_extension_manager.dir.statistics@g" $INI_FILE
+        sed -i "s@zend_extension_manager.extension.statistics@; [ST --zray-off] zend_extension_manager.extension.statistics@g" $INI_FILE
 fi
 
 yesnocommand "Confirm restart of Zend Server" "Zend Server needs to be restarted to apply the changes." "$ZCE_PREFIX/bin/zendctl.sh restart"
